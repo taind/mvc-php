54f6ff5bc1ccb05be0adf2ee9bd557b2ad3e1210
n.-.dt <taind@ndt.local>
2018-03-14 18:02:12 +0700

    doing view

--- webroot/index.php
+++ webroot/index.php
@@ -5,15 +5,15 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
-$router = new Router($_SERVER['REQUEST_URI']);
-
-echo '<pre>'; print_r('route: '.$router->getRoute().PHP_EOL);
-echo '<pre>'; print_r('Language: '.$router->getLanguage().PHP_EOL);
-echo '<pre>'; print_r('Controller: '.$router->getController().PHP_EOL);
-echo '<pre>'; print_r('Action: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
-
-echo "params: \r\n";
-print_r($router->getParams());
-
+//$router = new Router($_SERVER['REQUEST_URI']);
+//
+//echo '<pre>'; print_r('route: '.$router->getRoute().PHP_EOL);
+//echo '<pre>'; print_r('Language: '.$router->getLanguage().PHP_EOL);
+//echo '<pre>'; print_r('Controller: '.$router->getController().PHP_EOL);
+//echo '<pre>'; print_r('Action: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
+//
+//echo "params: \r\n";
+//print_r($router->getParams());
+App::run($_SERVER['REQUEST_URI']);
 
 ?>
