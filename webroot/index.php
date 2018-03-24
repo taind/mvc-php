bcf0a66d0bd4dd350d107fbc2da2d7c9d568a620
n.-.dt <taind@ndt.local>
2018-03-24 18:24:28 +0700

    doing uploads

--- webroot/index.php
+++ webroot/index.php
@@ -3,7 +3,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 define('VIEWS_PATH', ROOT.DS.'views');
-require_once(ROOT.DS.'lib'.DS.'init.php');
+require_once(ROOT.DS.'lib'.DS.'init.php'); //include init, trong init da include cac file can thiet
 
 session_start();
 
